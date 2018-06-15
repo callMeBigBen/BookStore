929158b0e65292eaa3141325b30d01ab96166ccf
huhaomeng <emhhm@outlook.com>
2018-06-15 17:59:43 +0800

    logAndReg
    logAndReg

--- src/main/java/bookStore/controller/UserController.java
+++ src/main/java/bookStore/controller/UserController.java
@@ -1,8 +1,43 @@
 package bookStore.controller;
 
 import org.springframework.stereotype.Controller;
+import bookStore.domain.User;
+import bookStore.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class UserController
-{
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("loginChecker")
+    public String log(@RequestParam("username") String username,
+                      @RequestParam("password") String password
+    ) {
+        User user = userService.getByUsername(username);
+        if (user != null) {
+            if (user.getPassword() == password) ;
+            return "redirect:index.jsp";
+        }
+        return null;
+    }
+
+    @RequestMapping("regToBookStore")
+    public String reg(@RequestParam("username") String username,
+                      @RequestParam("password") String password,
+                      @RequestParam("phone") String phone
+    ) {
+        User user = new User();
+        user.setPassword(password);
+        user.setUserName(username);
+        user.setPhone(phone);
+
+        userService.add(user);
+        return "redirect:index.jsp";
+    }
+
 }
--- src/main/java/bookStore/dao/UserDao.java
+++ src/main/java/bookStore/dao/UserDao.java
@@ -7,19 +7,84 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import bookStore.domain.User;
 
+import java.sql.ResultSet;
 import java.util.List;
 
 @Repository
-public class UserDao
-{
+public class UserDao {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    public List<User> list(){
+    public List<User> list() {
         String sql = "SELECT * FROM user";
 
         List<User> userServices = jdbcTemplate.query(sql, new BeanPropertyRowMapper(User.class));
 
         return userServices;
+    }
+
+    public User get(String id) {
+        String sql = "SELECT * FROM user WHERE id = " + id;
+
+        List<User> users = jdbcTemplate.query(sql, new BeanPropertyRowMapper(User.class));
+
+        User user = users.get(0);
+        return user;
+    }
+
+    public User getByUserName(String username) {
+        String sql = "SELECT * FROM user WHERE USERNAME = " + username;
+
+        List<User> users = jdbcTemplate.query(sql, new BeanPropertyRowMapper(User.class));
+
+        User user = users.get(0);
+        return user;
+    }
+
+    public void add(User user) {
+        String sql = "INSERT INTO user(id, userName, password, phone)" +
+                "Values(NULL, ?, ?, ?)";
+        Object[] args = new Object[]{
+                user.getUserName(), user.getPassword(), user.getPhone()
+        };
+
+        jdbcTemplate.update(sql, args);
+    }
+
+    public void modify(User user) {
+        String sql = "SELECT * FRMO user WHERE ID = " + user.getId();
+
+        User userOld = (User) jdbcTemplate.query(sql, new BeanPropertyRowMapper(User.class));
+
+        if (user.getUserName() != userOld.getUserName()) {
+            sql = "UPDATE user SET USERNAME = ? WHERE ID = ?";
+
+            Object[] args = new Object[]{
+                    user.getUserName(), user.getId()
+            };
+
+            jdbcTemplate.update(sql, args);
+        }
+
+        if (user.getPassword() != userOld.getPassword()) {
+            sql = "UPDATE user SET PASSWORD = ? WHERE ID = ?";
+
+            Object[] args = new Object[]{
+                    user.getPassword(), user.getId()
+            };
+
+            jdbcTemplate.update(sql, args);
+        }
+
+        if (user.getPhone() != userOld.getPhone()) {
+            sql = "UPDATE user SET PASSWORD = ? WHERE ID = ?";
+
+            Object[] args = new Object[]{
+                    user.getPhone(), user.getId()
+            };
+
+            jdbcTemplate.update(sql, args);
+        }
+
     }
 }
